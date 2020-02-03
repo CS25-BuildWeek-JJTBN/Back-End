759204052fd95751dc8251b5581e2906a701ac2e
Jordan <jordandoan@hotmail.com>
2020-02-03 13:11:24 -0800

    More changes

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -99,13 +99,12 @@ prod_db  =  dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 DATABASES['default'].update(prod_db)
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation

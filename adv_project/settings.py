6f327554a064057e45ae58ddc763fe3f860424a4
Jordan <jordandoan@hotmail.com>
2020-02-03 12:58:39 -0800

    Changing database?

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -98,12 +99,13 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 DATABASES['default'].update(prod_db)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation

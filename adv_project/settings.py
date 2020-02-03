d4f6ef4f152adf587f5719a7f8db0b72129c846b
Jordan <jordandoan@hotmail.com>
2020-02-03 13:28:01 -0800

    Fixed crashing locally

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.mmonMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'adv_project.urls'
